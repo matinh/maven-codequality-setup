66709fe07b93281c6f5e439204e1b2742b92117f
Martin Höller <martin.hoeller@xss.co.at>
2016-12-17 23:28:15 +0100

    - added checkstyle configuration and rules - adapted class App to match the rules

--- src/main/java/com/github/matinh/test/App.java
+++ src/main/java/com/github/matinh/test/App.java
@@ -4,10 +4,35 @@ package com.github.matinh.test;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    /** The name to say hello to. */
+    private String name;
+
+    /**
+     * Greet the caller.
+     */
+    private void sayHello()
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println("Hello " + (name == null ? "world" : name) + "!");
+    }
+
+    private void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Main method to execute the test-application.
+     * @param args arguments to the executable.
+     */
+    public static void main(String[] args)
+    {
+        final App app = new App();
+        app.setName("Martin");
+        app.sayHello();
+        app.setName("Lisi");
+        app.sayHello();
     }
 }
---
+++ src/main/java/com/github/matinh/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by martin on 12/17/16.
+ * <p>
+ * This is my test-package.
+ * </p>
+ */
+package com.github.matinh.test;
