4611b262121248778a70b140a137dce74a63dd0e
Martin Höller <martin.hoeller@xss.co.at>
2017-01-19 22:31:19 +0100

    - added findbugs config - updated checkstyle config - added PMD config

--- src/main/java/com/github/matinh/test/Greeter.java
+++ src/main/java/com/github/matinh/test/Greeter.java
@@ -11,11 +11,33 @@ public class Greeter
     /** The name to say hello to. */
     private String name;
 
+    private void notUsed()
+    {
+        System.out.printf("dead code");
+
+        String s = null;
+        // This would report an error from findbugs
+//        int someInt = 3;
+//        switch (someInt) {
+//        case 1: System.out.printf("1");
+//        case 2: System.out.printf("2");
+//        case 3: System.out.printf("3");
+//        }
+
+        if (s.isEmpty()) { // NPE, not yet detected
+            System.out.println("empty string!");
+        }
+
+    }
+
     /**
      * Greet the caller.
      */
     public void sayHello()
     {
+        if (false)
+            return;
+
         System.out.println("Hello " + (name == null ? "world" : name) + "!");
     }
 
